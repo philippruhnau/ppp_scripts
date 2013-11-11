0ed5ca8633efc9f1f1959cc5168917c7c9b35d86
philippruhnau <philipp.ruhnau@unitn.it>
2013-11-11 15:47:29 +0100

    add licence

--- change_dist.m
+++ change_dist.m
@@ -6,6 +6,22 @@ function new_dist = change_dist(x, new_mean, new_rg)
 %
 % CAVE: range is equally large in all columns afterwards
 
+% (c) copyright 2012. P.Ruhnau. philipp.ruhnau@yahoo.de
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 % definitions
 dif = zeros(1,size(x,2));
 ratio = dif;
