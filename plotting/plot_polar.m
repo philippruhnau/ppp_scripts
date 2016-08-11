52b6445589fdd44951fe2680b0f6de80c86e5cdb
Philipp Ruhnau <mail@philipp-ruhnau.de>
2016-08-11 10:48:55 +0200

    2016Aug11

--- plotting/plot_polar.m
+++ plotting/plot_polar.m
@@ -1,10 +1,26 @@
-function [h] = plot_polar(input)
+function [h] = plot_polar(input, rad, color, linewidth)
 
 % PLOT_POLAR(input)
 % wrapper to plot normalized rose plots
 %
 % input - vector of radians
+%
+% optional
+% rad - radius of the lines [1]
+% color - color of the lines ['k']
+% linewidth - [1]
 % 
 
+if nargin < 2, rad = 1; end
+if nargin < 3, color = 'k'; end
+if nargin < 4, linewidth = 1; end
 
-h = polar(repmat(input(:)', 2, 1), repmat([0 1]', 1, length(input)), 'k');
+h = polar(repmat(input(:)', 2, 1), repmat([0 rad]', 1, length(input)), color);
+
+
+
+for iL = 1:numel(h)
+  % change width
+  h(iL).LineWidth = linewidth;
+end
+
